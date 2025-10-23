aaf9f0f7ea45308388433f5e4d19362d70218f6d
Daniel Sampar <daniel_sampar@hotmail.com>
2025-10-23 00:08:58 -0300

    coords

--- src/data/quests.ts
+++ src/data/quests.ts
@@ -8,8 +8,8 @@ export const QUESTS: Quest[] = [
     riddle: 'Numa noite de rolê que nunca imaginei que mudaria tudo. Onde descobri que a sorte aparece nos lugares mais inesperados.',
     hint: 'É um lugar que eu sempre reclamava de ir, mas valeu a pena estar lá naquela noite.',
     targetLocation: {
-      latitude: -26.920886,
-      longitude: -49.0714982,
+      latitude: -26.8951684,
+      longitude: -49.0694597,
       accuracy: 50 // raio de 50 metros
     },
     completed: false,
@@ -23,8 +23,8 @@ export const QUESTS: Quest[] = [
     riddle: 'Onde celebramos sua primeira vitória profissional, onde meus olhos brilharam de orgulho e eu me apaixonei ainda mais pela mulher foda que você é.',
     hint: '🍣',
     targetLocation: {
-      latitude: -26.8990482,
-      longitude: -49.0802902,
+      latitude: -26.9221114,
+      longitude: -49.0663071,
       accuracy: 35
     },
     completed: false,
@@ -38,9 +38,9 @@ export const QUESTS: Quest[] = [
     riddle: 'Esse acho que nem precisa de dica. Bora logo que ainda ta no happy hour!',
     hint: 'Quase uma segunda casa.',
     targetLocation: {
-      latitude: -26.8993697,
-      longitude: -49.0798083,
-      accuracy: 40
+      latitude: -26.899685,
+      longitude: -49.0778829,
+      accuracy: 60
     },
     completed: false,
     difficulty: 'medium',
@@ -53,9 +53,9 @@ export const QUESTS: Quest[] = [
     riddle: 'Onde eu quase disse "eu te amo" pela primeira vez, porque meu coração transbordava, mas me segurei. Fiquei nervoso como nunca antes de te levar nesse lugar. Lá nós conversamos sobre tudo e eu descobri que te amava ainda mais.',
     hint: 'Infelizmente não dá mais para subir no terraço depois do jantar 🥲',
     targetLocation: {
-      latitude: -26.9041199,
-      longitude: -49.0777477,
-      accuracy: 30
+      latitude: -26.906988,
+      longitude: -49.0809675,
+      accuracy: 50
     },
     completed: false,
     difficulty: 'medium',
@@ -70,7 +70,7 @@ export const QUESTS: Quest[] = [
     targetLocation: {
       latitude: -26.9021614,
       longitude: -49.0785365,
-      accuracy: 20
+      accuracy: 50
     },
     completed: false,
     difficulty: 'hard',
