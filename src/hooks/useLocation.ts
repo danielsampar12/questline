b3d7e2bc806c410ca345858d226d45002e480a5e
Daniel Sampar <daniel_sampar@hotmail.com>
2025-10-23 03:19:41 -0300

    highest

--- src/hooks/useLocation.ts
+++ src/hooks/useLocation.ts
@@ -48,7 +48,7 @@ export const useLocation = (): UseLocationReturn => {
     try {
       // Use high accuracy options for better precision
       const locationResult = await Location.getCurrentPositionAsync({
-        accuracy: Location.Accuracy.BestForNavigation,
+        accuracy: Location.Accuracy.Highest,
       });
 
       const locationData: LocationData = {
